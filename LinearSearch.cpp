819050891d873580922c38b515e21a83c89b7439
Joseph Varghese <153506359+JosephNalkara@users.noreply.github.com>
2024-12-11 06:24:26 +0530

    Update floydwarshall.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,18 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void linearSearch(int array[],int x){
-for(int i=1;i<11;i++){
- if(array[i]==x){
-  cout<<"the element found at index:"<<i<<endl;
- }
- }}
-int main(){
-int c;
-int lst[10];
-cout << "Enter a 10 element array:" << endl;
-    for(int i = 0; i < 10; i++) {
-        cin >> lst[i];}
-cout<<"enter the element to be searched:"<<endl;
-cin>>c ;
-linearSearch(lst,c);
-return 0;}
+
+#define INF 99999  // Define a large value to represent infinity 
+
+void floydWarshall(int graph[][100], int n) {
+    int dist[100][100];
+
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    
+    for (int k = 0; k < n; k++) {          
+        for (int i = 0; i < n; i++) {      
+            for (int j = 0; j < n; j++) {  
+                
+                if (dist[i][k] != INF && dist[k][j] != 
+                INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+   
+    cout << "Shortest distances between every pair of vertices:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of vertices: ";
+    cin >> n;
+
+    int graph[100][100];
+    cout << "Enter the adjacency matrix (use " << INF << " for infinity):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    floydWarshall(graph, n);
+
+    return 0;
+}
